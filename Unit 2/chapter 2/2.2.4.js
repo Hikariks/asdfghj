19b462210c1057b30354fbf6ef173771c77d8e4e
xuan <xuanzhao@moonshotacademy.cn>
2024-03-15 11:16:51 +0800

    sd

--- Unit 2/chapter 2/2.2.4.js
+++ Unit 2/chapter 2/2.2.4.js
@@ -1,7 +1,9 @@
 let user = {
   firstName: "Ilya",
   sayHi() {
-    let arrow = console.log(this.firstName); 
+    let arrow = () => console.log(this.firstName); 
+    arrow()
   }
 };
 user.sayHi();
+
